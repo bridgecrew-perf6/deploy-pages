7b094866380bfea461223793cf9d60c6d3438429
寒潄 <winter.ld@alibaba-inc.com>
2022-04-14 10:55:58 +0800

    update

---
+++ src/pages/detail.js
@@ -0,0 +1,14 @@
+import { useNavigate } from "react-router-dom";
+
+export default () => {
+  let navigate = useNavigate();
+  return (
+    <div
+      onClick={() => {
+        navigate("/");
+      }}
+    >
+      "Detail PAGE"
+    </div>
+  );
+};
